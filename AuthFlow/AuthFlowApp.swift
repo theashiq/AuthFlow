7e08e8e1b26f51b5587a71be43720fb32b37134f
Mac Mini <>
2023-10-03 01:40:59 +0600

    Project Created

---
+++ AuthFlow/AuthFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthFlowApp.swift
+//  AuthFlow
+//
+//  Created by mac 2019 on 10/3/23.
+//
+
+import SwiftUI
+
+@main
+struct AuthFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AuthFlow/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AuthFlow
+//
+//  Created by mac 2019 on 10/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
